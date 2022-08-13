9191a193629fddf4f7ad5cad7a6e48bfa40db86a
Tish Faroul <lfaroul14@gmail.com>
2022-08-12 21:58:29 -0400

    able to perform get request on html page

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const fs = require("fs");
 const server = http.createServer((request, response) => {
     console.log(request.url, request.method);
 
-    response.setHeader("Content-Type", "text/plain");
+    response.setHeader("Content-Type", "text/html");
+
+    fs.readFile("./views/index.html", (error, data) => {
+        if(error){
+            console.log(error);
+            response.end();
+        } else {
+            response.write(data);
+            response.end();
+        }
+    })
 
 });
 
