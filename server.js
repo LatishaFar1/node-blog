6a91a1c4f4cd8f28308529719779e847305d7277
Tish Faroul <lfaroul14@gmail.com>
2022-08-15 09:50:31 -0400

    added ejs dependency

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const http = require("http");
-const fs = require("fs");
-const _ = require('lodash');
+// const http = require("http");
+// const fs = require("fs");
+// const _ = require('lodash');
 
-const server = http.createServer((request, response) => {
+// const server = http.createServer((request, response) => {
     
-    const greet = _.once(() => {
-        console.log('hi')
-    });
+//     const greet = _.once(() => {
+//         console.log('hi')
+//     });
 
-    response.setHeader("Content-Type", "text/html");
+//     response.setHeader("Content-Type", "text/html");
 
-    //ROUTING
-    let path = "./views/";
-    switch(request.url){
-        case "/":
-            path += "index.html"
-            response.statusCode = 200;
-            break;
-        case "/about":
-            path += "about.html";
-            response.statusCode = 200;
-            break;
+//     //ROUTING
+//     let path = "./views/";
+//     switch(request.url){
+//         case "/":
+//             path += "index.html"
+//             response.statusCode = 200;
+//             break;
+//         case "/about":
+//             path += "about.html";
+//             response.statusCode = 200;
+//             break;
 
-        case "/aboutme":
+//         case "/aboutme":
     
-            response.statusCode = 301;
-            response.setHeader("Location", "./about");
-            response.end();
-            break;
+//             response.statusCode = 301;
+//             response.setHeader("Location", "./about");
+//             response.end();
+//             break;
 
-        default:
-            path += "error.html";
-            response.statusCode = 404;
-            break;
-    }
+//         default:
+//             path += "error.html";
+//             response.statusCode = 404;
+//             break;
+//     }
 
 
-    //READING 
-    fs.readFile(path, (error, data) => {
-        if(error){
-            console.log(error);
-            response.end();
-        } else {
-            // below line is not necessary if we pass data into response.end()
-            // response.write(data);
-            response.end(data);
-        }
-    })
+//     //READING 
+//     fs.readFile(path, (error, data) => {
+//         if(error){
+//             console.log(error);
+//             response.end();
+//         } else {
+//             // below line is not necessary if we pass data into response.end()
+//             // response.write(data);
+//             response.end(data);
+//         }
+//     })
 
-});
+// });
 
-server.listen(3000, "localhost", () =>{
-    console.log("listening for requests on localhost 3000")
-})
+// server.listen(3000, "localhost", () =>{
+//     console.log("listening for requests on localhost 3000")
+// })
