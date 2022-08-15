994e2ac487babd6d46c91765be25a4663f42bf28
Tish Faroul <lfaroul14@gmail.com>
2022-08-15 12:26:39 -0400

    successfully passing some dynamic data

--- app.js
+++ app.js
@@ -16,7 +16,17 @@ app.get('/', (request, response) => {
     //send() automatically sets the content header and status code
 
     // response.sendFile('/views/index.html', {root: __dirname})
-    response.render('index');
+    
+    const posts = [
+        {title: "TitleONE", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Interdum velit laoreet id donec ultrices. "},
+        {title: "TitleTWO", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Interdum velit laoreet id donec ultrices. "},
+        {title: "TitleTHREE", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Interdum velit laoreet id donec ultrices. "},
+        {title: "TitleFOUR", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Interdum velit laoreet id donec ultrices. "}
+    ]
+    
+    response.render('index', { title: 'Home', posts});
+
+
 })
 
 app.get('/about', (request, response) => {
@@ -24,18 +34,18 @@ app.get('/about', (request, response) => {
 
     //if we want to return the file - we need to link to the directory name + file
     // response.sendFile('/views/about.html', {root: __dirname})
-    response.render('about')
+    response.render('about', { title: 'About'})
 })
 
 
 // //REDIRECTS
-
 // app.get('/about-me', (request, response) => {
 //     response.redirect('/about');
 // })
 
+
 app.get('/new', (request, response) => {
-    response.render('newpost');
+    response.render('newpost', { title: 'NEW POST'});
 })
 
 //ERROR PAGE - needs to be at the bottom of the page
